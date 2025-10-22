744ce86cb700788d5f07da9dc80dba8f74363665
AlexisV-prog <alexisvilley@gmail.com>
2025-10-22 11:58:07 +0200

    server init

--- Backend/index.js
+++ Backend/index.js
@@ -1,29 +1,18 @@
 // imports
+import "dotenv/config";
 import express from "express";
+//import { router } from "./src/router.js";
+
 
 export const app = express();
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
-  console.log(`🚀 l'api a démarré sur http://localhost:${PORT}/api`);
+// test Configuration
+app.get("/", (req, res) => {
+  res.send("Hello World");
 });
-//npm init -y
 
-
-//npm install express / dotenv / --save-dev eslint + ? js eslint globals / pg / 
-//npm install --save sequelize /npm install pg pg-hstore
-
-// npm run dev
-
-// Charger les variables d'environnement
-//npm install dotenv
-//mettre en premier
-
-
-
-// Importer les dépendances
-//npm install express
-
-
-
+app.listen(PORT, () => {
+  console.log(`🚀 l'api a démarré sur http://localhost:${PORT}`);
+});
