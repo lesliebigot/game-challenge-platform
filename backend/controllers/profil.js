6880ab70137b3cbfc5ef3397015f1c75508e35f3
AlexisV-prog <alexisvilley@gmail.com>
2025-10-22 14:36:59 +0200

    my challenges page road's done

---
+++ backend/controllers/profil.js
@@ -0,0 +1,26 @@
+import {Challenge} from "../models/challenge.js";
+import {User} from "../models/user.js";
+
+export const profilController = {
+  async getUserChallenges(req, res) {
+    const userId = req.user.id; // à récupérer via le middleware d'authenfication
+
+    const createdChallenges = await Challenge.findAll({
+      where: { creator_id: userId}
+    });
+
+    const likedChallenges = await Challenge.findAll({
+      include: [{
+        model: User,
+        where: { id: userId },
+        through: { attributes: [] } // table de liaison entre user et challenge à créer
+      }]
+    });
+
+    const registedChallenges = await Challenge.findAll({
+      where: { participant_id: userId}
+    });
+  
+    res.json(likedChallenges, createdChallenges, registedChallenges);
+  },
+};
--- backend/router.js
+++ backend/router.js
@@ -1,5 +1,10 @@
 import { Router } from "express";
+import { gameController } from "./controllers/game.js";
+import { challengeController } from "./controllers/challenge.js";
+import { profilController } from "./controllers/profil.js";
 
 export const router = Router();
 
-router.get("/");
+router.get("/", challengeController.getAll);
+router.get("/games", gameController.getAll);
+router.get("/profil/challenges", profilController.getUserChallenges);
