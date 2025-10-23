e7d45f27c21213061c06c4f8b551c3995b5b7b11
AlexisV-prog <alexisvilley@gmail.com>
2025-10-23 09:42:37 +0200

    zod correction

--- backend/schemas/userSchema.js
+++ backend/schemas/userSchema.js
@@ -4,8 +4,7 @@ export const userSignupSchema = z.object({
   username: z.string()
     .min(3, "Le nom d'utilisateur doit contenir au moins 3 caractères")
     .max(30, "Le nom d'utilisateur ne peut pas dépasser 30 caractères"),
-  email: z.string()
-    .email("Adresse email invalide")
+  email: z.email("Adresse email invalide")
     .max(100, "L'email est trop long"),
   password: z.string()
     .min(8, "Le mot de passe doit contenir au moins 8 caractères")
@@ -13,6 +12,6 @@ export const userSignupSchema = z.object({
 });
 
 export const userSigninSchema = z.object({
-  email: z.string().email("Adresse email invalide"),
+  email: z.email("Adresse email invalide"),
   password: z.string().min(1, "Le mot de passe est requis"),
 });
