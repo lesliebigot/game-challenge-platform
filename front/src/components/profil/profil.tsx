3a42c75105b0427a837e418522d38d1f5d81d5b3
Sammy-Mekrez <sammy.mekrez@oclock.school>
2025-10-31 09:25:29 +0100

    FIX: update CSRF token SignUp/Profil

--- front/src/components/profil/profil.tsx
+++ front/src/components/profil/profil.tsx
@@ -1,13 +1,24 @@
 import axios from "axios";
-import { useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router";
 import useUserContext from "../../context/useUserContext";
+import { useEffect, useState } from "react";
 
-export function Profil(){
+export function Profil() {
   const { logout } = useUserContext();
-  
+
   // Récupération directe dans localStorage (attention à la synchro avec contexte)
   const userIdFromStorage = localStorage.getItem("userId");
   const userId = userIdFromStorage ? Number(userIdFromStorage) : null;
+
+  const [csrfToken, setCsrfToken] = useState(""); // CSRF token
+  // Récupérer le token CSRF au montage
+  useEffect(() => {
+    axios
+      .get("http://localhost:3000/api/csrf-token", { withCredentials: true })
+      .then((res) => {
+        setCsrfToken(res.data.csrfToken);
+      });
+  }, []);
 
   const navigate = useNavigate();
   const deleteUser = async () => {
@@ -19,15 +30,19 @@ export function Profil(){
       // on envoie au back sur le endpoint /signup les credentials
       const response = await axios.delete(
         `http://localhost:3000/users/${userId}`,
+        {
+          headers: { "X-CSRF-Token": csrfToken },
+          withCredentials: true,
+        }
       );
       console.log("Suppression de l'utilisateur", response);
-    } catch(e){
+    } catch (e) {
       console.error("Erreur signup:", e instanceof Error ? e.message : e);
     }
   };
 
   const handleSubmit = async (e: React.FormEvent) => {
-    e.preventDefault();   // Empêche le rechargement de page
+    e.preventDefault(); // Empêche le rechargement de page
     if (!userId) {
       alert("Impossible de supprimer : utilisateur non identifié.");
       return;
@@ -38,65 +53,123 @@ export function Profil(){
     navigate("/");
   };
 
-
   return (
-    <>
-      <div className="h-full">
-        <h1 className="text-3xl font-bold mb-6 pixel-font text-center mt-5">Mon profil</h1>
-        <div className="grid grid-cols-1 content-center gap-5 mb-5 m-auto lg:max-w-2xl md:max-w-2xl sm:max-w-sm max-w-xs">
-          {/* Formulaire de modification */}
-          <form action="" className="fieldset bg-base-200 border-base-300 rounded-box border p-4 text-center">
-            <div className="grid grid-cols-1 sm:grid-cols-1 md:grid-cols-3 gap-7">
-              <fieldset className="max-w-96">
-                <legend className="fieldset-legend">Email</legend>
+    <div className="h-full">
+      <h1 className="text-3xl font-bold mb-6 pixel-font text-center mt-5">
+        Mon profil
+      </h1>
+      <div className="grid grid-cols-1 content-center gap-5 mb-5 m-auto lg:max-w-2xl md:max-w-2xl sm:max-w-sm max-w-xs">
+        {/* Formulaire de modification */}
+        <form
+          action=""
+          className="fieldset bg-base-200 border-base-300 rounded-box border p-4 text-center"
+        >
+          <div className="grid grid-cols-1 sm:grid-cols-1 md:grid-cols-3 gap-7">
+            <fieldset className="max-w-96">
+              <legend className="fieldset-legend">Email</legend>
 
-                <label htmlFor="oldEmail" className="label">Ancien email</label>
-                <input id="oldEmail" name="oldEmail" type="email" className="input" placeholder="Ancien email" />
+              <label htmlFor="oldEmail" className="label">
+                Ancien email
+              </label>
+              <input
+                id="oldEmail"
+                name="oldEmail"
+                type="email"
+                className="input"
+                placeholder="Ancien email"
+              />
 
-                <label htmlFor="newEmail" className="label">Nouveau email</label>
-                <input id="newEmail" name="newEmail" type="email" className="input" placeholder="Nouveau email" />
-              </fieldset>
-            
-              <fieldset className="max-w-96">
-                <legend className="fieldset-legend">Mot de passe</legend>
-
-                <label htmlFor="oldPassword" className="label">Ancien mot de passe</label>
-                <input id="oldPassword" name="oldPassword" type="password" className="input" placeholder="Ancien mot de passe" />
-
-                <label htmlFor="newPassword" className="label">Nouveau mot de passe</label>
-                <input id="newPassword" name="newPassword" type="password" className="input" placeholder="Nouveau mot de passe" />    
-
-              </fieldset>
-
-              <fieldset className="max-w-96" >
-                <legend className="fieldset-legend">Pseudo</legend>
-
-                <label htmlFor="oldPseudo" className="label">Ancien pseudo</label>
-                <input id="oldPseudo" name="oldPseudo" type="text" className="input" placeholder="Ancien pseudo" />
-          
-                <label htmlFor="newPseudo" className="label">Nouveau pseudo</label>
-                <input id="newPseudo" name="newPseudo"type="text" className="input" placeholder="Nouveau pseudo" />
-
-              </fieldset>
-            </div>
-            <div >
-              <button className="btn btn-neutral mt-4">Modifier</button>
-            </div>
-          </form>
-
-          {/* Formulaire de suppression */}
-          <form onSubmit={handleSubmit} className="flex justify-center fieldset bg-base-200 border-base-300 rounded-box  border p-4 text-center w-full max-w-sm mx-auto">
-            <fieldset className="max-w-80">
-              <legend className="fieldset-legend">Supprimer le compte</legend>
-              <label htmlFor="password" className="label">Mot de passe</label>
-              <input id="password" name="password" type="password" className="input" placeholder="Mot de passe" />
-
-              <button className="btn btn-error mt-4">Supprimer mon compte</button>
+              <label htmlFor="newEmail" className="label">
+                Nouveau email
+              </label>
+              <input
+                id="newEmail"
+                name="newEmail"
+                type="email"
+                className="input"
+                placeholder="Nouveau email"
+              />
             </fieldset>
-          </form>
-         
-        </div>
+
+            <fieldset className="max-w-96">
+              <legend className="fieldset-legend">Mot de passe</legend>
+
+              <label htmlFor="oldPassword" className="label">
+                Ancien mot de passe
+              </label>
+              <input
+                id="oldPassword"
+                name="oldPassword"
+                type="password"
+                className="input"
+                placeholder="Ancien mot de passe"
+              />
+
+              <label htmlFor="newPassword" className="label">
+                Nouveau mot de passe
+              </label>
+              <input
+                id="newPassword"
+                name="newPassword"
+                type="password"
+                className="input"
+                placeholder="Nouveau mot de passe"
+              />
+            </fieldset>
+
+            <fieldset className="max-w-96">
+              <legend className="fieldset-legend">Pseudo</legend>
+
+              <label htmlFor="oldPseudo" className="label">
+                Ancien pseudo
+              </label>
+              <input
+                id="oldPseudo"
+                name="oldPseudo"
+                type="text"
+                className="input"
+                placeholder="Ancien pseudo"
+              />
+
+              <label htmlFor="newPseudo" className="label">
+                Nouveau pseudo
+              </label>
+              <input
+                id="newPseudo"
+                name="newPseudo"
+                type="text"
+                className="input"
+                placeholder="Nouveau pseudo"
+              />
+            </fieldset>
+          </div>
+          <div>
+            <button className="btn btn-neutral mt-4">Modifier</button>
+          </div>
+        </form>
+
+        {/* Formulaire de suppression */}
+        <form
+          onSubmit={handleSubmit}
+          className="flex justify-center fieldset bg-base-200 border-base-300 rounded-box  border p-4 text-center w-full max-w-sm mx-auto"
+        >
+          <fieldset className="max-w-80">
+            <legend className="fieldset-legend">Supprimer le compte</legend>
+            <label htmlFor="password" className="label">
+              Mot de passe
+            </label>
+            <input
+              id="password"
+              name="password"
+              type="password"
+              className="input"
+              placeholder="Mot de passe"
+            />
+
+            <button className="btn btn-error mt-4">Supprimer mon compte</button>
+          </fieldset>
+        </form>
       </div>
-    </>
+    </div>
   );
 }
--- front/src/components/signUp/signUp.tsx
+++ front/src/components/signUp/signUp.tsx
@@ -1,23 +1,30 @@
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
-{/* Inscription */}
-
-export function SignUp(){
-
+export function SignUp() {
   const [pseudo, setPseudo] = useState("");
   const [lastname, setLastname] = useState("");
   const [firstname, setFirstname] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [confirmedPassword, setConfirmedPassword] = useState("");
-  const [birthdate, setBirthdate] = useState(""); 
+  const [birthdate, setBirthdate] = useState("");
+  const [csrfToken, setCsrfToken] = useState(""); // CSRF token
   const navigate = useNavigate();
-  
+
+  // Récupérer le token CSRF au montage
+  useEffect(() => {
+    axios
+      .get("http://localhost:3000/api/csrf-token", { withCredentials: true })
+      .then((res) => {
+        setCsrfToken(res.data.csrfToken);
+      });
+  }, []);
+
   const signup = async () => {
     try {
-      // on envoie au back sur le endpoint /signup les credentials
+      // Inclusion du token CSRF dans les headers
       const response = await axios.post(
         "http://localhost:3000/register",
         {
@@ -27,14 +34,18 @@ export function SignUp(){
           email,
           password,
           birthdate: birthdate.toString(),
-          "role_id":"1",
+          role_id: "1",
         },
+        {
+          headers: { "X-CSRF-Token": csrfToken },
+          withCredentials: true,
+        }
       );
       console.log("Réponse inscription :", response);
       navigate("/signin");
-    } catch(e){
+    } catch (e) {
       console.error("Erreur signup:", e instanceof Error ? e.message : e);
-    };
+    }
   };
 
   const handleSubmit = (e: React.FormEvent) => {
@@ -48,64 +59,118 @@ export function SignUp(){
     signup();
   };
 
-
   return (
-    <> 
-      <div className="w-full max-w-4xl mx-auto px-4 pt-10 pb-10">
-        <h1 className="text-2xl sm:text-3xl font-bold mb-6 pixel-font text-center">Page d'inscription</h1>
-        <form onSubmit={handleSubmit}>
-          <fieldset className="fieldset bg-base-200 border-base-300 rounded-box w-full border p-4 sm:p-6">
-            <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
-
-              <div>
-                <label htmlFor="lastname" className="label">Nom</label>
-                <input  id="lastname" name="lastname" type="text" className="input w-full" placeholder="Nom" value={lastname}
-                  onChange={(e) => setLastname(e.target.value)}/>
-              </div>
-
-              <div>
-                <label htmlFor="firstname" className="label">Prénom</label>
-                <input id="firstname" name="firstname" type="text" className="input w-full" placeholder="Prénom" value={firstname}
-                  onChange={(e) => setFirstname(e.target.value)}/>
-              </div>
-
-              <div>
-                <label htmlFor="birthdate" className="label">Date de naissance</label>
-                <input id="birthdate" name="birthdate" type="date" className="input w-full" placeholder="Date de naissance" value={birthdate}
-                  onChange={(e) => setBirthdate(e.target.value)} />
-              </div>
-              
-              <div>
-                <label htmlFor="email" className="label">Email</label>
-                <input id="email" name="email" type="email" className="input w-full" placeholder="Email" value={email}
-                  onChange={(e) => setEmail(e.target.value)}/>
-              </div>
-
-              <div>
-                <label htmlFor="password" className="label">Mot de passe</label>
-                <input id="password" name="password" type="password" className="input w-full" placeholder="Mot de passe" value={password}
-                  onChange={(e) => setPassword(e.target.value)}/>
-              </div>
-              
-              <div>
-                <label htmlFor="confirmedPassword" className="label">Confirmation de mot de passe</label>
-                <input id="confirmedPassword" name="confirmedPassword" type="password" className="input w-full" placeholder="Mot de passe" value={confirmedPassword}
-                  onChange={(e) => setConfirmedPassword(e.target.value)}/>
-              </div>
-
-              <div>
-                <label htmlFor="pseudo"  className="label">Pseudo</label>
-                <input id="pseudo" name="pseudo" type="text" className="input w-full" placeholder="Pseudo" value={pseudo}
-                  onChange={(e) => setPseudo(e.target.value)}/>
-              </div>
+    <div className="w-full max-w-4xl mx-auto px-4 pt-10 pb-10">
+      <h1 className="text-2xl sm:text-3xl font-bold mb-6 pixel-font text-center">
+        Page d'inscription
+      </h1>
+      <form onSubmit={handleSubmit}>
+        <fieldset className="fieldset bg-base-200 border-base-300 rounded-box w-full border p-4 sm:p-6">
+          <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+            <div>
+              <label htmlFor="lastname" className="label">
+                Nom
+              </label>
+              <input
+                id="lastname"
+                name="lastname"
+                type="text"
+                className="input w-full"
+                placeholder="Nom"
+                value={lastname}
+                onChange={(e) => setLastname(e.target.value)}
+              />
             </div>
-
-            <div className="flex justify-center mt-6">
-              <button className="btn btn-primary">S'inscrire</button>
+            <div>
+              <label htmlFor="firstname" className="label">
+                Prénom
+              </label>
+              <input
+                id="firstname"
+                name="firstname"
+                type="text"
+                className="input w-full"
+                placeholder="Prénom"
+                value={firstname}
+                onChange={(e) => setFirstname(e.target.value)}
+              />
             </div>
-          </fieldset>
-        </form>
-      </div>
-    </>
+            <div>
+              <label htmlFor="birthdate" className="label">
+                Date de naissance
+              </label>
+              <input
+                id="birthdate"
+                name="birthdate"
+                type="date"
+                className="input w-full"
+                placeholder="Date de naissance"
+                value={birthdate}
+                onChange={(e) => setBirthdate(e.target.value)}
+              />
+            </div>
+            <div>
+              <label htmlFor="email" className="label">
+                Email
+              </label>
+              <input
+                id="email"
+                name="email"
+                type="email"
+                className="input w-full"
+                placeholder="Email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
+            </div>
+            <div>
+              <label htmlFor="password" className="label">
+                Mot de passe
+              </label>
+              <input
+                id="password"
+                name="password"
+                type="password"
+                className="input w-full"
+                placeholder="Mot de passe"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
+            </div>
+            <div>
+              <label htmlFor="confirmedPassword" className="label">
+                Confirmation de mot de passe
+              </label>
+              <input
+                id="confirmedPassword"
+                name="confirmedPassword"
+                type="password"
+                className="input w-full"
+                placeholder="Mot de passe"
+                value={confirmedPassword}
+                onChange={(e) => setConfirmedPassword(e.target.value)}
+              />
+            </div>
+            <div>
+              <label htmlFor="pseudo" className="label">
+                Pseudo
+              </label>
+              <input
+                id="pseudo"
+                name="pseudo"
+                type="text"
+                className="input w-full"
+                placeholder="Pseudo"
+                value={pseudo}
+                onChange={(e) => setPseudo(e.target.value)}
+              />
+            </div>
+          </div>
+          <div className="flex justify-center mt-6">
+            <button className="btn btn-primary">S'inscrire</button>
+          </div>
+        </fieldset>
+      </form>
+    </div>
   );
 }
